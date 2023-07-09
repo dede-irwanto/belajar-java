181fcf9b71a0fc1d2d79d27ec855a8152f604096
dede-irwanto <id.dedeirwanto@gmail.com>
2023-07-10 00:22:13 +0800

    Latihan ketiga

---
+++ src/hari1/TipeDataChar.java
@@ -0,0 +1,13 @@
+package hari1;
+
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char e = 'E';
+        char k = 'K';
+        char a = 'A';
+
+        System.out.println(e);
+        System.out.println(k);
+        System.out.println(a);
+    }
+}
