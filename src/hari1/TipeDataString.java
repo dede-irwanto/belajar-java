8065cf38882ea3fd8e68aafadf30c56076947aea
dede-irwanto <id.dedeirwanto@gmail.com>
2023-07-10 00:34:30 +0800

    Latihan 5

---
+++ src/hari1/TipeDataString.java
@@ -0,0 +1,13 @@
+package hari1;
+
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Dede";
+        String lastName = "Irwanto";
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(fullName);
+    }
+}
