19929014618120df07048c84d124bb73a085ba0c
dede-irwanto <id.dedeirwanto@gmail.com>
2023-07-10 00:15:41 +0800

    Latihan kedua bagian b

--- src/hari1/KonversiTipeDataNumber.java
+++ src/hari1/KonversiTipeDataNumber.java
@@ -2,13 +2,13 @@ package hari1;
 
 public class KonversiTipeDataNumber {
     public static void main(String[] args) {
-        // Widening Casting (Otomatis): byte -> short -> int -> long -> float -> double
+        // Widening Casting (Otomatis) konversi dari size kecil ke besar: byte -> short -> int -> long -> float -> double
         int iniInteger = 10;
         long iniLong = iniInteger;
         float iniFloat = iniLong;
         double iniDouble = iniFloat;
 
-        // Narrowing Casting (Manual): double -> float -> long -> int -> short -> byte
+        // Narrowing Casting (Manual) konversi dari size besar ke kecil: double -> float -> long -> int -> short -> byte
         int iniInteger2 = (int) iniDouble;
     }
 }
