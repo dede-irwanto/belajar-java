331d7271707dfb8a6a4a1cf1f3a27178193cabd2
dede-irwanto <id.dedeirwanto@gmail.com>
2023-07-10 01:52:22 +0800

    Latihan 10

---
+++ src/hari1/OperasiPerbandingan.java
@@ -0,0 +1,15 @@
+package hari1;
+
+public class OperasiPerbandingan {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 100;
+
+        System.out.println(a > b); // false
+        System.out.println(a < b); // false
+        System.out.println(a >= b); // true
+        System.out.println(a <= b); // true
+        System.out.println(a == b); // true
+        System.out.println(a != b); // false
+    }
+}
