8526d228cebee2a04c03d4212bd04ed0416bd6ec
dede-irwanto <id.dedeirwanto@gmail.com>
2023-07-10 23:10:42 +0800

    Latihan 12

--- src/hari2/MethodParameter.java
+++ src/hari2/MethodParameter.java
@@ -3,6 +3,7 @@ package hari2;
 public class MethodParameter {
     public static void main(String[] args) {
         sayHello("Dede", "Irwanto");
+        sayHello("Eka", "Potabuga");
     }
 
     static void sayHello(String firstName, String lastName) {
