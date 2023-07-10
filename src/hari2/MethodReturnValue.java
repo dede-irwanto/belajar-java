0cdd92d0e7f780c870c857f20dbdf5d15e7dfe51
dede-irwanto <id.dedeirwanto@gmail.com>
2023-07-10 23:20:18 +0800

    Latihan 13

--- src/hari2/MethodReturnValue.java
+++ src/hari2/MethodReturnValue.java
@@ -2,12 +2,28 @@ package hari2;
 
 public class MethodReturnValue {
     public static void main(String[] args) {
-        var result = sum(10, 10);
-        System.out.println(result);
+        System.out.println(sum(100, 120));
+
+        System.out.println(hitung(100,"-", 20));
+        System.out.println(hitung(100,"+", 50));
+        System.out.println(hitung(100,"salah", 50));
     }
 
     static int sum(int value1, int value2) {
         var total = value1 + value2;
+        return total;
+    }
+
+    static int hitung(int value1, String operasi, int value2) {
+        int total = switch (operasi) {
+            case "+":
+                yield value1 + value2;
+            case "-":
+                yield value1 - value2;
+            default:
+                yield 0;
+        };
+
         return total;
     }
 }
