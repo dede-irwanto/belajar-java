aea9522727bfddb2769ea90e71739c8c3bdd2db1
dede-irwanto <id.dedeirwanto@gmail.com>
2023-07-11 00:20:40 +0800

    Latihan 18

---
+++ src/hari2/Komentar.java
@@ -0,0 +1,18 @@
+package hari2;
+
+public class Komentar {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Method untuk menjumlahkan value1 dan value2 (contoh komenter multiple line)
+     * @param value1 angka pertama
+     * @param value2 angka kedua
+     * @return hasil penjumlahan value1 dan value2
+     */
+    static int sum(int value1, int value2) {
+        // jumlahkan value1 dan value2 (contoh komentar single line)
+        return value1 + value2;
+    }
+}
