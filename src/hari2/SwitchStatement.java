66e959f8e6ec9d000cf3867ce789ead4a4e2bd58
dede-irwanto <id.dedeirwanto@gmail.com>
2023-07-10 20:41:35 +0800

    Latihan 3

---
+++ src/hari2/SwitchStatement.java
@@ -0,0 +1,34 @@
+package hari2;
+
+public class SwitchStatement {
+    public static void main(String[] args) {
+        var nilai = "E";
+
+        switch (nilai) {
+            case "A":
+                System.out.println("Nilai anda istimewa");
+                break;
+            case "B":
+                System.out.println("Nilai anda baik");
+                break;
+            case "C":
+                System.out.println("Nilai anda cukup");
+                break;
+            case "D":
+                System.out.println("Anda tidak lulus");
+                break;
+            default:
+                System.out.println("Anda salah jurusan");
+        }
+
+        // gaya lain penulisan switch case
+        var nilai2 = "C";
+        switch (nilai2) {
+            case "A" -> System.out.println("Nilai anda istimewa");
+            case "B" -> System.out.println("Nilai anda baik");
+            case "C" -> System.out.println("Nilai anda cukup");
+            case "D" -> System.out.println("Anda tidak lulus");
+            default -> System.out.println("Anda salah jurusan");
+        }
+    }
+}
