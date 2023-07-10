60114f88b7dfad6c41a2ab6b24c35a9f14135630
dede-irwanto <id.dedeirwanto@gmail.com>
2023-07-10 20:56:05 +0800

    Latihan 3

--- src/hari2/SwitchStatement.java
+++ src/hari2/SwitchStatement.java
@@ -21,14 +21,53 @@ public class SwitchStatement {
                 System.out.println("Anda salah jurusan");
         }
 
-        // gaya lain penulisan switch case (hanya berjalan di java 14+)
-        var nilai2 = "C";
-        switch (nilai2) {
+        // penulisan switch case dengan lambda (hanya berjalan di java 14+)
+        nilai = "C";
+        switch (nilai) {
             case "A" -> System.out.println("Nilai anda istimewa");
             case "B" -> System.out.println("Nilai anda baik");
             case "C" -> System.out.println("Nilai anda cukup");
             case "D" -> System.out.println("Anda tidak lulus");
             default -> System.out.println("Anda salah jurusan");
         }
+
+        // kata kunci yield
+
+        // contoh tanpa yield
+        nilai = "A";
+        String ucapan;
+
+        switch (nilai) {
+            case "A" -> ucapan = "Nilai anda istimewa";
+            case "B" -> ucapan = "Nilai anda baik";
+            case "C" -> ucapan = "Nilai anda cukup";
+            case "D" -> ucapan = "Anda tidak lulus";
+            default -> ucapan = "Anda salah jurusan";
+        }
+
+        System.out.println(ucapan);
+
+        // contoh dengan yield
+        nilai = "B";
+        ucapan = switch (nilai) {
+            case "A":
+                yield "Nilai anda istimewa";
+            case "B":
+                yield "Nilai anda baik";
+            case "C":
+                yield "Nilai anda cukup";
+            case "D":
+                yield "Anda tidak lulus";
+            default:
+                yield "Anda salah jurusan";
+        };
+
+        System.out.println(ucapan);
+
+
+
+
+
+
     }
 }
